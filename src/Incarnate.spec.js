b5b1abcbf27eed6499c1bce2f87a4954ae50b97f
Ryan Graff <ryan.graff@gstv.com>
2016-12-20 14:42:05 -0500

    test(Incarnate) Add context specific dependency resolution tests.

--- src/Incarnate.spec.js
+++ src/Incarnate.spec.js
@@ -4,6 +4,7 @@ import Incarnate from './Incarnate';
 const MOCK_INSTANCE = {};
 const MOCK_DEPENDENCY = {};
 const MOCK_DEPENDENCY_DEPENDENCY = {};
+const MOCK_CTX_PROP_VALUE = 'MOCK_CTX_PROP_VALUE';
 
 module.exports = {
   'Incarnate': {
@@ -69,6 +70,40 @@ module.exports = {
       }, {});
 
       expect(instance).toBe(MOCK_DEPENDENCY_DEPENDENCY);
+    },
+    'should resolve a context specific dependency': () => {
+      const instance = Incarnate('mock', {
+        'mock-dep-dep': {
+          args: [],
+          factory: () => {
+            return MOCK_DEPENDENCY_DEPENDENCY;
+          }
+        },
+        'mock-dep': {
+          args: [
+            'mock-dep-dep',
+            ctx => ctx.mockCtxProp
+          ],
+          factory: (mockDepDep, mockCtxProp) => {
+            return {
+              a: mockDepDep,
+              b: mockCtxProp
+            };
+          }
+        },
+        'mock': {
+          args: [
+            'mock-dep'
+          ],
+          factory: (mockDep) => {
+            return mockDep.b;
+          }
+        }
+      }, {
+        mockCtxProp: MOCK_CTX_PROP_VALUE
+      });
+
+      expect(instance).toBe(MOCK_CTX_PROP_VALUE);
     }
   }
 };

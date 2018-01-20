a31255fc7eb715c66bd6a3015892bf6930c8d336
Ryan Graff <ryan.graff@bedrockdata.com>
2018-01-20 10:26:36 -0500

    feat: (HashMatrix) Add set and unset checks to optimize changes.

--- src/HashMatrix.jsx
+++ src/HashMatrix.jsx
@@ -103,80 +103,86 @@ export default class HashMatrix {
   }
 
   setPath(path, value) {
-    // TODO: DO NOT set if the value is exactly equal UNLESS the path WAS NOT set.
-    const newHashMatrix = {
-      ...this.hashMatrix
-    };
-    const pathArray = this.getPathArray(path);
-    const lastIndex = pathArray.length - 1;
-    const lastPart = pathArray[lastIndex];
+    // TRICKY: DO NOT set if the value is exactly equal UNLESS the path WAS NOT set.
+    if (this.pathIsSet(path) && value === this.getPath(path)) {
+      return;
+    } else {
+      const newHashMatrix = {
+        ...this.hashMatrix
+      };
+      const pathArray = this.getPathArray(path);
+      const lastIndex = pathArray.length - 1;
+      const lastPart = pathArray[lastIndex];
 
-    let currentValue = newHashMatrix;
+      let currentValue = newHashMatrix;
 
-    for (let i = 0; i < lastIndex; i++) {
-      const part = pathArray[i];
-      const nextPart = pathArray[i + 1];
+      for (let i = 0; i < lastIndex; i++) {
+        const part = pathArray[i];
+        const nextPart = pathArray[i + 1];
 
-      // TRICKY: Build out the tree is it's not there.
-      if (!currentValue.hasOwnProperty(part)) {
-        currentValue[part] = HashMatrix.keyIsNumeric(nextPart) ? [] : {};
-      } else if (currentValue[part] instanceof Array) {
-        currentValue[part] = [
-          ...currentValue[part]
-        ];
-      } else if (currentValue[part] instanceof Object) {
-        currentValue[part] = {
-          ...currentValue[part]
-        };
+        // TRICKY: Build out the tree is it's not there.
+        if (!currentValue.hasOwnProperty(part)) {
+          currentValue[part] = HashMatrix.keyIsNumeric(nextPart) ? [] : {};
+        } else if (currentValue[part] instanceof Array) {
+          currentValue[part] = [
+            ...currentValue[part]
+          ];
+        } else if (currentValue[part] instanceof Object) {
+          currentValue[part] = {
+            ...currentValue[part]
+          };
+        }
+
+        currentValue = currentValue[part];
       }
 
-      currentValue = currentValue[part];
-    }
+      currentValue[lastPart] = value;
 
-    currentValue[lastPart] = value;
+      this.hashMatrix = newHashMatrix;
 
-    this.hashMatrix = newHashMatrix;
+      // Notify lifecycle listeners of changes all the way up the path.
+      if (this.onPathChange instanceof Function && pathArray.length) {
+        const currentPath = [...pathArray];
 
-    // Notify lifecycle listeners of changes all the way up the path.
-    if (this.onPathChange instanceof Function && pathArray.length) {
-      const currentPath = [...pathArray];
-
-      // TRICKY: Start with the deepest path and move up to the most shallow.
-      while (currentPath.length) {
-        this.onPathChange(this.getPathString(currentPath));
-        currentPath.pop();
+        // TRICKY: Start with the deepest path and move up to the most shallow.
+        while (currentPath.length) {
+          this.onPathChange(this.getPathString(currentPath));
+          currentPath.pop();
+        }
       }
     }
   }
 
   unsetPath(path) {
-    // TODO: DO NOT unset if the path WAS NOT set.
-    const pathArray = this.getPathArray(path);
-    const {parentPath, name} = this.getPathInfo(pathArray) || {};
+    // TRICKY: DO NOT unset if the path WAS NOT set.
+    if (this.pathIsSet(path)) {
+      const pathArray = this.getPathArray(path);
+      const {parentPath, name} = this.getPathInfo(pathArray) || {};
 
-    if (parentPath instanceof Array && parentPath.length) {
-      const parentObject = this.getPath(parentPath);
+      if (parentPath instanceof Array && parentPath.length) {
+        const parentObject = this.getPath(parentPath);
 
-      if (parentObject instanceof Object && parentObject.hasOwnProperty(name)) {
-        const newParentObject = {
-          ...parentObject
+        if (parentObject instanceof Object && parentObject.hasOwnProperty(name)) {
+          const newParentObject = {
+            ...parentObject
+          };
+
+          delete newParentObject[name];
+
+          this.setPath(parentPath, newParentObject);
+        }
+      } else if (typeof name === 'string' && this.hashMatrix.hasOwnProperty(name)) {
+        const newHashMatrix = {
+          ...this.hashMatrix
         };
 
-        delete newParentObject[name];
+        delete newHashMatrix[name];
 
-        this.setPath(parentPath, newParentObject);
-      }
-    } else if (typeof name === 'string' && this.hashMatrix.hasOwnProperty(name)) {
-      const newHashMatrix = {
-        ...this.hashMatrix
-      };
+        this.hashMatrix = newHashMatrix;
 
-      delete newHashMatrix[name];
-
-      this.hashMatrix = newHashMatrix;
-
-      if (this.onPathChange instanceof Function) {
-        this.onPathChange(name);
+        if (this.onPathChange instanceof Function) {
+          this.onPathChange(name);
+        }
       }
     }
   }
